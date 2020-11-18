c99d1812d05c9927c8c8ea1c9d91b5cf9495d20f
Vitaliy Nasypov <nasvit25@gmail.com>
2020-11-18 23:07:50 +0700

    Task #408727

---
+++ chapter_007/src/main/java/ru/job4j/concurrent/ParseFile.java
@@ -0,0 +1,42 @@
+package ru.job4j.concurrent;
+
+import java.io.*;
+
+public class ParseFile {
+    private File file;
+
+    public synchronized void setFile(File f) {
+        file = f;
+    }
+
+    public synchronized File getFile() {
+        return file;
+    }
+
+    public synchronized String getContent() throws IOException {
+        InputStream i = new FileInputStream(file);
+        StringBuilder output = new StringBuilder();
+        int data;
+        while ((data = i.read()) > 0) {
+            output.append((char) data);
+        }
+        return output.toString();
+    }
+
+    public synchronized String getContentWithoutUnicode() throws IOException {
+        InputStream i = new FileInputStream(file);
+        StringBuilder output = new StringBuilder();
+        int data;
+        while ((data = i.read()) > 0) {
+            if (data < 0x80) {
+                output.append((char) data);
+            }
+        }
+        return output.toString();
+    }
+
+    public synchronized void saveContent(String content) throws IOException {
+        OutputStream o = new FileOutputStream(file);
+        o.write(content.getBytes());
+    }
+}
