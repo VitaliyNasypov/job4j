3dc0f7461fe6b8ea6e0c20cc53fb6e07f1fdae25
Vitaliy Nasypov <nasvit25@gmail.com>
2020-11-18 22:03:11 +0700

    Task #408721

---
+++ chapter_007/src/main/java/ru/job4j/concurrent/Node.java
@@ -0,0 +1,27 @@
+package ru.job4j.concurrent;
+
+public class Node<T> {
+    private final Node next;
+    private final T value;
+
+    public Node(Node next, T value) {
+        this.next = next;
+        this.value = value;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public T getValue() {
+        return value;
+    }
+
+//    public void setNext(Node next) {
+//        this.next = next;
+//    }
+//
+//    public void setValue(T value) {
+//        this.value = value;
+//    }
+}
