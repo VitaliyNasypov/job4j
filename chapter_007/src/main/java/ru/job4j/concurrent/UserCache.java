100ad96f8969f1846f94c21969dc952ca4cb38a4
Vitaliy Nasypov <nasvit25@gmail.com>
2020-11-19 18:21:24 +0700

    Task #408722

---
+++ chapter_007/src/main/java/ru/job4j/concurrent/UserCache.java
@@ -0,0 +1,52 @@
+package ru.job4j.concurrent;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class UserCache {
+    private final ConcurrentHashMap<Integer, User> users = new ConcurrentHashMap<>();
+    private final AtomicInteger id = new AtomicInteger();
+
+    public void add(User user) {
+        users.put(id.incrementAndGet(), User.of(user.getName()));
+    }
+
+    public User findById(int id) {
+        return User.of(users.get(id).getName());
+    }
+
+    public List<User> findAll() {
+        ArrayList<User> copyUsers = new ArrayList<>();
+        users.forEach((key, value) -> copyUsers.add(User.of(value.getName())));
+        return copyUsers;
+    }
+}
+
+class User {
+    private int id;
+    private String name;
+
+    public static User of(String name) {
+        User user = new User();
+        user.name = name;
+        return user;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
