96af0a228740c5240b970650d6c1fd75b501bfc0
Vitaliy Nasypov <nasvit25@gmail.com>
2020-11-19 21:57:44 +0700

    Fixed Task #408712

--- chapter_007/src/main/java/ru/job4j/concurrent/Wget.java
+++ chapter_007/src/main/java/ru/job4j/concurrent/Wget.java
@@ -5,45 +5,93 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.URL;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class Wget {
-    private static final Logger logger = Logger.getLogger(Wget.class.getName());
+    private static final Logger LOGGER = Logger.getLogger(Wget.class.getName());
 
     public static void main(String[] args) {
-        if (args.length == 2
-                && args[0].matches("^(https?|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]")
-                && args[1].matches("\\d+")) {
-            String[] nameTempFile = args[0].split("\\.");
-            File fileTemp = new File("fileTemp." + nameTempFile[nameTempFile.length - 1]);
-            try (BufferedInputStream inputStream = new BufferedInputStream(new URL(args[0]).openStream());
-                 FileOutputStream fileOutputStream = new FileOutputStream(fileTemp, true)) {
-                byte[] buffer = new byte[1000];
-                int bytesRead;
-                long speedLimit = Long.parseLong(args[1]);
-                long startTime = System.nanoTime();
-                long diffTime = 0;
-                int count = 0;
-                while ((bytesRead = inputStream.read(buffer, 0, buffer.length)) != -1) {
-                    fileOutputStream.write(buffer, 0, bytesRead);
-                    diffTime += System.nanoTime() - startTime;
-                    count++;
-                    if (diffTime / 1_000_000 < 1000 && count == speedLimit) {
-                        Thread.sleep(1000 - diffTime / 1_000_000);
-                        count = 0;
-                        diffTime = 0;
-                    } else if (count == speedLimit) {
-                        count = 0;
-                        diffTime = 0;
-                    }
-                    startTime = System.nanoTime();
-                }
-            } catch (IOException | InterruptedException e) {
-                logger.log(Level.SEVERE, "Exception: ", e);
+        if (inquiryVerification(args)) {
+            Callable<String> downloader = new Downloader(args[0], Long.parseLong(args[1]));
+            FutureTask<String> future = new FutureTask<>(downloader);
+            new Thread(future).start();
+            try {
+                LOGGER.info("Результат загрузки: " + future.get());
+            } catch (InterruptedException | ExecutionException e) {
+                LOGGER.severe("Exception: " + e);
             }
         } else {
-            System.out.println("Неверные параметры");
+            LOGGER.info("Неверные параметры");
         }
+    }
+
+    private static boolean inquiryVerification(String[] args) {
+        return args.length == 2
+                && args[0].matches(
+                "^(https?|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]")
+                && args[1].matches("\\d+");
+    }
+}
+
+class Downloader implements Callable<String> {
+    private static final Logger LOGGER = Logger.getLogger(Downloader.class.getName());
+    private final String url;
+    private final long speedLimit;
+
+    Downloader(String url, long speedLimit) {
+        this.speedLimit = speedLimit;
+        this.url = url;
+    }
+
+    @Override
+    public String call() {
+        return fileUpload();
+    }
+
+    private String fileUpload() {
+        File fileTemp = generationTempFileName();
+        try (BufferedInputStream inputStream = new BufferedInputStream(new URL(url).openStream());
+             FileOutputStream fileOutputStream = new FileOutputStream(fileTemp, true)) {
+            byte[] buffer = new byte[1000];
+            int bytesRead;
+            long diffTime = 0;
+            int countBytesRead = 0;
+            long startTime = System.nanoTime();
+            while ((bytesRead = inputStream.read(buffer, 0, buffer.length)) != -1) {
+                fileOutputStream.write(buffer, 0, bytesRead);
+                diffTime += System.nanoTime() - startTime;
+                countBytesRead += bytesRead;
+                if (checkDownloadSpeed(diffTime, countBytesRead)) {
+                    countBytesRead = 0;
+                    diffTime = 0;
+                }
+                startTime = System.nanoTime();
+            }
+            return "Файл загружен";
+        } catch (IOException e) {
+            LOGGER.log(Level.SEVERE, "Exception: ", e);
+            return "Файл не загружен";
+        }
+    }
+
+    private File generationTempFileName() {
+        String[] nameTempFile = url.split("\\.");
+        return new File("fileTemp." + nameTempFile[nameTempFile.length - 1]);
+    }
+
+    private boolean checkDownloadSpeed(long diffTime, int countBytesRead) {
+        if (diffTime / 1_000_000 < 1000 && countBytesRead >= speedLimit / 1000) {
+            try {
+                Thread.sleep(1000 - diffTime / 1_000_000);
+                return true;
+            } catch (InterruptedException e) {
+                LOGGER.log(Level.SEVERE, "Exception: ", e);
+            }
+        }
+        return countBytesRead >= speedLimit;
     }
 }
---
+++ chapter_007/src/test/java/ru/job4j/concurrent/WgetTest.java
@@ -0,0 +1,80 @@
+package ru.job4j.concurrent;
+
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.io.File;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.stream.Stream;
+
+public class WgetTest {
+
+    @Test
+    public void shouldRequestNoCorrect() {
+        String url = "test://raw.githubusercontent.com/peterarsentev/course_test/master/pom.xml";
+        String speedLimit = "aaa";
+        Wget.main(new String[]{url,speedLimit});
+        File file = new File("fileTemp.xml");
+        Assertions.assertFalse(file.exists());
+    }
+    @Test
+    public void shouldRequestCorrect() {
+        String url = "https://raw.githubusercontent.com/peterarsentev/course_test/master/pom.xml";
+        String speedLimit = "200";
+        Wget.main(new String[]{url,speedLimit});
+        File file = new File("fileTemp.xml");
+        Assertions.assertTrue(file.exists());
+        file.delete();
+    }
+}
+
+class DownloaderTest {
+    private static final Logger LOGGER = Logger.getLogger(Downloader.class.getName());
+
+    private static Stream<Callable<String>> correctUrl() {
+        return Stream.of(
+                new Downloader("https://raw.githubusercontent.com/peterarsentev/course_test/master/pom.xml",
+                        200));
+    }
+
+    private static Stream<Callable<String>> noCorrectUrl() {
+        return Stream.of(
+                new Downloader("https://raw.githubusercontent.com/peterarsentev/course_test/master",
+                        200));
+    }
+
+    @ParameterizedTest
+    @MethodSource("noCorrectUrl")
+    public void fileNotShouldUploaded(Downloader downloader) {
+        FutureTask<String> future = new FutureTask<>(downloader);
+        new Thread(future).start();
+        String result = null;
+        try {
+            result = future.get();
+        } catch (InterruptedException | ExecutionException e) {
+            Assertions.assertTrue(LOGGER.isLoggable(Level.SEVERE));
+        }
+        Assertions.assertEquals("Файл не загружен", result);
+        File file = new File("fileTemp.xml");
+        Assertions.assertFalse(file.exists());
+    }
+
+    @ParameterizedTest
+    @MethodSource("correctUrl")
+    public void fileShouldUploaded(Downloader downloader) throws ExecutionException, InterruptedException {
+        FutureTask<String> future = new FutureTask<>(downloader);
+        new Thread(future).start();
+        String result = future.get();
+        Assertions.assertEquals("Файл загружен", result);
+        File file = new File("fileTemp.xml");
+        Assertions.assertTrue(file.exists());
+        file.delete();
+    }
+
+}
