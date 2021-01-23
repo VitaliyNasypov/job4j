8533c4b2cfe29a9333949af53084e7509643c9a3
Vitaliy Nasypov <nasvit25@gmail.com>
2021-01-23 18:32:26 +0700

    Refactoring

--- job4j_tracker/src/main/java/ru/job4j/tracker/Item.java
+++ job4j_tracker/src/main/java/ru/job4j/tracker/Item.java
@@ -78,9 +78,9 @@ public class Item {
 
     @Override
     public String toString() {
-        return "Item{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                '}';
+        return "Item{"
+                + "id=" + id
+                + ", name='" + name + '\''
+                + '}';
     }
 }
